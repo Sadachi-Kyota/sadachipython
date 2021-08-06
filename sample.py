b1c68403c160415688ba4dcf5e64d48e0aadd310
Sadachi Kyota <tobotoboman@gmail.com>
2021-08-06 14:52:38 +0900

    initial commit

---
+++ sample.py
@@ -0,0 +1,57 @@
+
+# print("Hello World")
+
+# i = 0
+
+# if i == 0 :
+#     print("iは1です")
+# else:
+#     print("iは" +i+"です")
+
+# def greet(data):
+#     term = data.split(" ")
+#     print(term)
+
+# sentence = "I am a robot."
+# greet(sentence)
+# 大文字小文字
+# print("Hello World".upper())
+# print("HELLO WORLD".lower())
+
+# 大文字とスプリットの組み合わせ
+# print("Hello World.I am Tack".upper().split(" "))
+# 変数と変数の組み合わせ
+# x = "Hello World"
+# print(x.upper())
+
+# 変数に変数を入れる
+# x = "Good morning"
+# x = x.upper()
+# print(x)
+
+# リストのタイプを確認
+# print(type(["Hello"]))
+
+# リストの作成
+# print(["Hello","World"])
+# print(["a","b","c","d","e"])
+# print(["a","b","c",1,2,3])
+# print(["a","b","c",["d",1,2,3]])
+
+# リストの編集、インデックス番号の指定
+# # print(["a","b","c","d"][10])
+# print(["a","b","c",["d",1,2,3]][3][1:3])
+# x = ["a","b","c"]
+# x[1] = "MMM"
+# print(x)
+
+# appendメソッドの練習
+# x = ["a","b","c"]
+# x.append("d")
+# print(x)
+
+
+# print(("Hello","World"))
+# print(type(("Hello",)))
+# print((1,2,3,["a","b","c"]))
+
